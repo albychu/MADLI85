76ae33d0d4e806ddce6ef036c69894b90a78fce0
Albert C <albertchu94@outlook.com>
2020-12-15 18:37:08 -0500

    MadLibs.py

---
+++ pythonProject/MadLibs.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+
+# import stories  <---- MUST DO
+
+
+"""
+This script is intended to show my project and what I learned in class.
+
+Author | Albert Chu
+
+"""
+
+# Welcoming Code
+print()
+print("Welcome to the world of MADLI85.")
+print("Fill in the blanks to many of your childhood stories!")
+print("Use your silly, funny, and outrageous imagination!")
+print(input("PRESS ENTER TO BEGIN YOUR CHILDHOOD ADVENTURES."))
+
+
+
+adj = input("Adjective: " )
+verb = input("Verb: " )
+noun = input("Noun: " )
+fav_food = input("Favorite Food: " )
+dream_goal = input("Dream Goal: ")
+
+madlib = f"Python has been a {adj} these past few days! I can't wait to keep {verb}. \
+I love {noun}, {fav_food}, and {dream_goal}."
+
+
+#with open('None') as my_none_file:
+ #   contents = my_none_file.read()
+
+
+
+#story_finder()
+#favorite = input("Is this one of your favorite stories? Type In CAPS YES/NO: ")
+
+#while True:
+ #   if favorite.upper() == 'NO':
+  #      story_finder()
+   #     favorite = input("Is this one of your favorite stories? In CAPS YES/NO: ")
+    #    print()
+  #  elif favorite.upper() == 'YES':
+   #     print()
+    #    break
+    # elif favorite.upper() != 'YES' or 'NO':
+      #  print("Invalid Input. Please Type In CAPS YES/NO")
+       # favorite = input("Is this one of your favorite stories? In CAPS YES/NO: ")
+
---
+++ pythonProject/main.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+
+# import stories  <---- access api
+# import sys <--- to stop from breaking
+
+
+"""
+This script is intended to show my project and what I learned in class.
+
+Author | Albert Chu
+
+"""
+
+# Welcoming Code
+print("Intoduction:\n"
+    "Welcome to the world of MADLI85.\n"
+    "Fill in the blanks to many of your childhood stories!\n"
+    "Use your silly, funny, and outrageous imagination!\n")
+print(input("PRESS ENTER TO BEGIN YOUR CHILDHOOD ADVENTURES."))
+
+
+
+adj = input("Adjective: " )
+verb = input("Verb: " )
+noun = input("Noun: " )
+fav_food = input("Favorite Food: " )
+dream_goal = input("Dream Goal: ")
+
+madlib = f"Python has been a {adj} these past few days! I can't wait to keep {verb}. \
+I love {noun}, {fav_food}, and {dream_goal}."
+
+
+
+
+
+
+#story_finder()
+#favorite = input("Is this one of your favorite stories? Type In CAPS YES/NO: ")
+
+#while True:
+ #   if favorite.upper() == 'NO':
+  #      story_finder()
+   #     favorite = input("Is this one of your favorite stories? In CAPS YES/NO: ")
+    #    print()
+  #  elif favorite.upper() == 'YES':
+   #     print()
+    #    break
+    # elif favorite.upper() != 'YES' or 'NO':
+      #  print("Invalid Input. Please Type In CAPS YES/NO")
+       # favorite = input("Is this one of your favorite stories? In CAPS YES/NO: ")
+
